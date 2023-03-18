56cfa44e78c8f478a60e2a64a8c0ee0d2dfbd9c4
wasifuddin <79798771+wasifuddin@users.noreply.github.com>
2023-03-19 03:38:05 +0600

    Update vr_sharing.py

--- vr_sharing.py
+++ vr_sharing.py
@@ -1,20 +1,31 @@
+import sys
 import cv2
-from flask import Flask, Response, request
+from flask import Flask, Response
+import requests
 import numpy as np
 import cvzone
 app = Flask(__name__)
-from PIL import ImageGrab
+# from PIL import ImageGrab
+# If operating system is macOS or Windows import ImageGrab
+if sys.platform == 'darwin' or sys.platform.startswith('win'):
+    from PIL import ImageGrab
+else:
+    import pyscreenshot
+
 screen_size = (1920, 1080)
+
 
 def gen_frames():
     while True:
-
-        img = np.array(ImageGrab.grab(bbox=(0, 0, screen_size[0], screen_size[1])))
+        if sys.platform == 'darwin' or sys.platform.startswith('win'):
+            img = np.array(ImageGrab.grab(
+                bbox=(0, 0, screen_size[0], screen_size[1])))
+        else:
+            img = np.array(pyscreenshot.grab())
         img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-        imgList = [img,img]
+        imgList = [img, img]
         img = cvzone.stackImages(imgList, 2, 0.5)
-        cv2.line(img, (970,0), (970,1920),(0,0,0), 30)
-        print(img.shape)
+        cv2.line(img, (970, 0), (970, 1920), (0, 0, 0), 30)
         ret, buffer = cv2.imencode('.jpg', img)
         frame = buffer.tobytes()
         yield (b'--frame\r\n'
@@ -30,10 +41,10 @@ def video_feed():
 def vr_mode_run():
     app.run(host='0.0.0.0', port=5000)
 
+
 def vr_mode_exit():
     response = requests.post('http://192.168.0.106:5000/shutdown')
     print(response.text)
-
 
 
 # 192.168.0.106
